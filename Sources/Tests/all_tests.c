0b5a02d21b85499a3a413986efe30f390a869a6b
Arun Kumar <arun.kumar@red-gate.com>
2015-11-03 13:58:42 +0000

    Added Or to binary map

--- Sources/Tests/all_tests.c
+++ Sources/Tests/all_tests.c
@@ -4,6 +4,7 @@ static void RunAllTests(void)
 {
     RUN_TEST_GROUP(Point);
     RUN_TEST_GROUP(Array);
+    RUN_TEST_GROUP(BinaryMap);
 
     RUN_TEST_GROUP(VotingFilter);
     RUN_TEST_GROUP(LocalHistogram);
