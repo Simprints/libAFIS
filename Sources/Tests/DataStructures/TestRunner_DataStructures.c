d90daca522b025f4ce5d60ef5a32686a773e16bd
Bob Arnott <bob.arnott@red-gate.com>
2015-11-03 13:10:28 +0000

    Add new tests for sanity checking the data structures

---
+++ Sources/Tests/DataStructures/TestRunner_DataStructures.c
@@ -0,0 +1,8 @@
+#include "unity.h"
+#include "unity_fixture.h"
+
+TEST_GROUP_RUNNER(DataStructures)
+{
+    RUN_TEST_CASE(DataStructures, BlockMap_SanityCheck);
+    RUN_TEST_CASE(DataStructures, Image_SanityCheck);
+}
---
+++ Sources/Tests/DataStructures/Test_BlockMap.c
@@ -0,0 +1,35 @@
+#include "unity.h"
+#include "unity_fixture.h"
+#include "Utils/IO/ArrayIO.h"
+#include "Utils/IO/BlockMapIO.h"
+#include "Utils/IO/ImageIO.h"
+
+#include <stdio.h>
+#define PRINT_OUT_DATA
+
+TEST_GROUP(DataStructures);
+
+TEST_SETUP(DataStructures)
+{
+}
+
+TEST_TEAR_DOWN(DataStructures)
+{
+}
+
+TEST(DataStructures, BlockMap_SanityCheck)
+{
+    BlockMap blockMap = BlockMapIO_ConstructFromFile("DataStructures/101_1.tif.12.Equalize.blocks.dat");
+#ifdef PRINT_OUT_DATA    
+    BlockMapIO_Printf(&blockMap);
+#endif    
+}
+
+
+TEST(DataStructures, Image_SanityCheck)
+{
+    UInt8Array2D image = ImageIO_ConstructFromFile("DataStructures/101_1.tif.12.Equalize.image.dat");
+#ifdef PRINT_OUT_DATA
+    ImageIO_Printf(&image);
+#endif
+}
