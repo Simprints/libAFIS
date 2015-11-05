18e94eac9cf0fbd83faac348e7e931198be7299d
Mark Raymond <markr@raymonds.org.uk>
2015-11-05 13:20:27 +0000

    Rename SkeletonBuilderRidge -> Ridge.

--- Sources/Extraction/Model/Minutia.c
+++ Sources/Extraction/Model/Minutia.c
@@ -6,21 +6,21 @@ List* Minutia_GetRidges(const Minutia *me)
     return (List *) &me->ridges;
 }
 
-void Minutia_AttachStart(Minutia *me, SkeletonBuilderRidge *ridge)
+void Minutia_AttachStart(Minutia *me, Ridge *ridge)
 {
     if (!List_ContainsData(&me->ridges, ridge))
     {
         List_AddData(&me->ridges, ridge);
-        SkeletonBuilderRidge_SetStart(ridge, me);
+        Ridge_SetStart(ridge, me);
     }
 }
 
-void Minutia_DetachStart(Minutia *me, SkeletonBuilderRidge *ridge)
+void Minutia_DetachStart(Minutia *me, Ridge *ridge)
 {
     if (List_ContainsData(&me->ridges, ridge))
     {
         List_RemoveData(&me->ridges, ridge);
-        if (SkeletonBuilderRidge_GetStart(ridge) == me)
-            SkeletonBuilderRidge_SetStart(ridge, NULL);
+        if (Ridge_GetStart(ridge) == me)
+            Ridge_SetStart(ridge, NULL);
     }
 }
