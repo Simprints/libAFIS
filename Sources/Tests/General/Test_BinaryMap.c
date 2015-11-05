74454f08eefc8967d70db3e06fffd2f414580257
Jeff Foster <jeff.foster@red-gate.com>
2015-11-05 09:02:25 +0000

    some failing tests, but need to check why

--- Sources/Tests/General/Test_BinaryMap.c
+++ Sources/Tests/General/Test_BinaryMap.c
@@ -131,3 +131,50 @@ TEST(BinaryMap, BinaryMap_AndNot)
     TEST_ASSERT_EQUAL_INT_MESSAGE(1, BinaryMap_GetBit(&source,1,2), "Failed at: 1,2");
     TEST_ASSERT_EQUAL_INT_MESSAGE(1, BinaryMap_GetBit(&source,2,2), "Failed at: 2,2");
 }
+
+TEST(BinaryMap, BinaryMap_AndArea)
+{
+   BinaryMap source = BinaryMap_Construct(3,3);
+
+    BinaryMap_SetBitOne(&source,0,0);
+    BinaryMap_SetBitOne(&source,1,0);
+    BinaryMap_SetBitZero(&source,0,1);
+    BinaryMap_SetBitZero(&source,1,1);
+
+    BinaryMap_SetBitOne(&source,2,0);
+    BinaryMap_SetBitZero(&source,2,1);
+    BinaryMap_SetBitOne(&source,0,2);
+    BinaryMap_SetBitOne(&source,1,2);
+    BinaryMap_SetBitOne(&source,2,2);
+
+    BinaryMap target = BinaryMap_Construct(3,3);
+
+    BinaryMap_SetBitZero(&target,0,0);
+    BinaryMap_SetBitZero(&target,1,0);
+    BinaryMap_SetBitOne(&target,0,1);
+    BinaryMap_SetBitOne(&target,1,1);
+
+    BinaryMap_SetBitZero(&target,2,0);
+    BinaryMap_SetBitOne(&target,2,1);
+    BinaryMap_SetBitZero(&target,0,2);
+    BinaryMap_SetBitZero(&target,1,2);
+    BinaryMap_SetBitZero(&target,2,2);
+    
+    RectangleC rect = RectangleC_ConstructFrom4Ints(0,0,1,1);
+    Point origin = Point_Construct(0,0);
+
+    BinaryMap_AndArea(&source,  &target, &rect, &origin);
+
+    // Area being "and"
+    TEST_ASSERT_EQUAL_INT_MESSAGE(0, BinaryMap_GetBit(&source,0,0), "Failed at: 0,0");
+    TEST_ASSERT_EQUAL_INT_MESSAGE(0, BinaryMap_GetBit(&source,1,0), "Failed at: 1,0");
+    TEST_ASSERT_EQUAL_INT_MESSAGE(0, BinaryMap_GetBit(&source,0,1), "Failed at: 0,1");
+    TEST_ASSERT_EQUAL_INT_MESSAGE(0, BinaryMap_GetBit(&source,1,1), "Failed at: 1,1");
+
+    // Should be original values
+    TEST_ASSERT_EQUAL_INT_MESSAGE(0, BinaryMap_GetBit(&source,2,1), "Failed at: 2,1");
+    TEST_ASSERT_EQUAL_INT_MESSAGE(1, BinaryMap_GetBit(&source,2,0), "Failed at: 2,0");
+    TEST_ASSERT_EQUAL_INT_MESSAGE(1, BinaryMap_GetBit(&source,0,2), "Failed at: 0,2");
+    TEST_ASSERT_EQUAL_INT_MESSAGE(1, BinaryMap_GetBit(&source,1,2), "Failed at: 1,2");
+    TEST_ASSERT_EQUAL_INT_MESSAGE(1, BinaryMap_GetBit(&source,2,2), "Failed at: 2,2");
+}
--- Sources/Tests/General/runners/TestRunner_BinaryMap.c
+++ Sources/Tests/General/runners/TestRunner_BinaryMap.c
@@ -6,4 +6,5 @@ TEST_GROUP_RUNNER(BinaryMap)
     RUN_TEST_CASE(BinaryMap, BinaryMap_Or);
     RUN_TEST_CASE(BinaryMap, BinaryMap_And);
     RUN_TEST_CASE(BinaryMap, BinaryMap_AndNot);
+    RUN_TEST_CASE(BinaryMap, BinaryMap_AndArea);
 }
