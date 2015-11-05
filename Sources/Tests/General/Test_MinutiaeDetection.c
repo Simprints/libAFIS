f5060791166d6e2da579ad09248755daaa66a175
Andrew Farries <andrew.farries@red-gate.com>
2015-11-05 12:23:59 +0000

    Implementation of ridge ending detection

--- Sources/Tests/General/Test_MinutiaeDetection.c
+++ Sources/Tests/General/Test_MinutiaeDetection.c
@@ -12,7 +12,26 @@ TEST_TEAR_DOWN(MinutiaeDetection)
 {
 }
 
+static bool CountNeighbors(int x, int y, int image[3][3]) {
+  int acc = 0;
+
+  if ( x > 0 )          acc += image[x-1][y];
+  if ( x > 0 && y < 2 ) acc += image[x-1][y+1];
+  if ( y < 2 )          acc += image[x][y+1];
+  if ( x < 2 && y < 2 ) acc += image[x+1][y+1];
+  if ( x < 2 )          acc += image[x+1][y];
+  if ( x < 2 && y > 0 ) acc += image[x+1][y-1];
+  if ( y > 0 )          acc += image[x][y-1];
+  if ( x > 0 && y > 0 ) acc += image[x-1][y-1];
+
+  return acc;
+}
+
 static bool DetectRidgeEnding(int image[3][3]) {
+  for ( int i=0; i < 3; ++i )
+    for ( int j=0; j < 3; ++j )
+      if ( CountNeighbors(i, j, image) == 1 )
+        return true;
   return false;
 }
 
