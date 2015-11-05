1952ea59a439133b43f2494d0b08906770ca43c7
Jeff Foster <jeff.foster@acm.org>
2015-11-05 16:25:45 +0000

    best match skipper protos

---
+++ Sources/Matcher/BestMatchSkipper.c
@@ -0,0 +1,23 @@
+#include "Matcher/BestMatchSkipper.h"
+
+struct BestMatchSkipper BestMatchSkipper_Construct(int32_t persons, int32_t skip)
+{
+  struct BestMatchSkipper r;
+
+  return r;
+}
+
+void BestMatchSkipper_AddScore(const BestMatchSkipper* me, int32_t person, float score)
+{
+}
+
+
+float BestMatchSkipper_GetSkipScore(const BestMatchSkipper* me, int32_t person)
+{
+  return 1.0f;
+}
+
+PersonsSkipScore* GetSortedScores(const BestMatchSkipper* me, int* size)
+{
+  return 0;
+}
