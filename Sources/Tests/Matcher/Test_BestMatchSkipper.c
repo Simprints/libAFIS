279ae33923de09cf7472be9624f78c0c41533db5
Jeff Foster <jeff.foster@acm.org>
2015-11-05 16:31:29 +0000

    an empty test

---
+++ Sources/Tests/Matcher/Test_BestMatchSkipper.c
@@ -0,0 +1,18 @@
+#include "Matcher/BestMatchSkipper.h"
+#include "unity.h"
+#include "unity_fixture.h"
+
+TEST_GROUP(BestMatchSkipper);
+
+TEST_SETUP(BestMatchSkipper)
+{
+}
+
+TEST_TEAR_DOWN(BestMatchSkipper)
+{
+}
+
+
+TEST(BestMatchSkipper, BestMatchSkipper_Construct)
+{
+}
---
+++ Sources/Tests/Matcher/runners/TestRunner_BestMatchSkipper.c
@@ -0,0 +1,7 @@
+#include "unity.h"
+#include "unity_fixture.h"
+
+TEST_GROUP_RUNNER(BestMatchSkipper)
+{
+    RUN_TEST_CASE(BestMatchSkipper, BestMatchSkipper_Construct);
+}
