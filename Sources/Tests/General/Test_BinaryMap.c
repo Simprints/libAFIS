d966a0a6aa3e4c796d07d2f53170e03abe175ce4
Jeff Foster <jeff.foster@red-gate.com>
2015-11-05 10:41:33 +0000

    Assert behaviour against C# example.

--- Sources/Tests/General/Test_BinaryMap.c
+++ Sources/Tests/General/Test_BinaryMap.c
@@ -143,11 +143,7 @@ TEST(BinaryMap, BinaryMap_AndArea)
     */
     BinaryMap_SetBitOne(&source,0,0);
     BinaryMap_SetBitOne(&source,1,0);
-    BinaryMap_SetBitZero(&source,0,1);
-    BinaryMap_SetBitZero(&source,1,1);
-
     BinaryMap_SetBitOne(&source,2,0);
-    BinaryMap_SetBitZero(&source,2,1);
     BinaryMap_SetBitOne(&source,0,2);
     BinaryMap_SetBitOne(&source,1,2);
     BinaryMap_SetBitOne(&source,2,2);
@@ -159,31 +155,31 @@ TEST(BinaryMap, BinaryMap_AndArea)
          ***
          ---
      */
-    BinaryMap_SetBitZero(&target,0,0);
-    BinaryMap_SetBitZero(&target,1,0);
     BinaryMap_SetBitOne(&target,0,1);
     BinaryMap_SetBitOne(&target,1,1);
-
-    BinaryMap_SetBitZero(&target,2,0);
     BinaryMap_SetBitOne(&target,2,1);
-    BinaryMap_SetBitZero(&target,0,2);
-    BinaryMap_SetBitZero(&target,1,2);
-    BinaryMap_SetBitZero(&target,2,2);
     
     RectangleC rect = RectangleC_ConstructFrom4Ints(0,0,1,1);
     Point origin = Point_Construct(0,0);
 
     BinaryMap_AndArea(&source,  &target, &rect, &origin);
 
+    printf("\n\n");
+    for (int i=0;i<3;++i) {
+      for (int j=0;j<3;++j) {
+        printf("%d", BinaryMap_GetBit(&source,j,i));
+      }
+      printf("\n");
+    }
+    printf("\nBAR\n");
+
     // Area being "and"
     TEST_ASSERT_EQUAL_INT_MESSAGE(0, BinaryMap_GetBit(&source,0,0), "Failed at: 0,0");
     TEST_ASSERT_EQUAL_INT_MESSAGE(0, BinaryMap_GetBit(&source,1,0), "Failed at: 1,0");
+    TEST_ASSERT_EQUAL_INT_MESSAGE(0, BinaryMap_GetBit(&source,2,0), "Failed at: 2,0");
     TEST_ASSERT_EQUAL_INT_MESSAGE(0, BinaryMap_GetBit(&source,0,1), "Failed at: 0,1");
     TEST_ASSERT_EQUAL_INT_MESSAGE(0, BinaryMap_GetBit(&source,1,1), "Failed at: 1,1");
-
-    // Should be original values
     TEST_ASSERT_EQUAL_INT_MESSAGE(0, BinaryMap_GetBit(&source,2,1), "Failed at: 2,1");
-    TEST_ASSERT_EQUAL_INT_MESSAGE(1, BinaryMap_GetBit(&source,2,0), "Failed at: 2,0");
     TEST_ASSERT_EQUAL_INT_MESSAGE(1, BinaryMap_GetBit(&source,0,2), "Failed at: 0,2");
     TEST_ASSERT_EQUAL_INT_MESSAGE(1, BinaryMap_GetBit(&source,1,2), "Failed at: 1,2");
     TEST_ASSERT_EQUAL_INT_MESSAGE(1, BinaryMap_GetBit(&source,2,2), "Failed at: 2,2");
