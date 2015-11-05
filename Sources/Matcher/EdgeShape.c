07c63799fd07dbf6a360a3a6d660cc792cb20359
Jeff Foster <jeff.foster@acm.org>
2015-11-05 15:56:50 +0000

    try to create the structure.  Goal one finish edge shape

---
+++ Sources/Matcher/EdgeShape.c
@@ -0,0 +1,8 @@
+#include "Matcher/EdgeShape.h"
+
+struct EdgeShape* EdgeShape_Construct(
+  const struct FingerprintTemplate* template,
+  int reference, int neighbour)
+{
+  return 0;
+}
---
+++ Sources/Matcher/FingerprintTemplate.h
@@ -0,0 +1,8 @@
+#ifndef _FINGERPRINTTEMPLATE_H_
+#define _FINGERPRINTTEMPLATE_H_
+
+struct FingerprintTemplate {
+};
+
+#endif
+
