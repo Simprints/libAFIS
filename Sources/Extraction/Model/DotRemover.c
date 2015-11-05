54897b6336be6d537e9548d5048f098053f56417
Mark Raymond <markr@raymonds.org.uk>
2015-11-05 12:39:37 +0000

    Rename.

--- Sources/Extraction/Model/DotRemover.c
+++ Sources/Extraction/Model/DotRemover.c
@@ -5,16 +5,16 @@
 
 void DotRemover_Filter(SkeletonBuilder *skeleton)
 {
-    List *list = SkeletonBuilder_GetMinutiae(skeleton);
+    List *minutiae = SkeletonBuilder_GetMinutiae(skeleton);
 
-    for (ListElement *i = list->head; i != NULL; i = i->next)
+    for (ListElement *i = minutiae->head; i != NULL; i = i->next)
     {
         SkeletonBuilderMinutia *minutia = (SkeletonBuilderMinutia *) i->data;
         assert(minutia != NULL);
 
         if (List_GetCount(SkeletonBuilderMinutia_GetRidges(minutia)) == 0)
         {
-            int32_t ret = List_Remove(list, i, NULL);
+            int32_t ret = List_Remove(minutiae, i, NULL);
             assert(ret == 0);
 
             SkeletonBuilderMinutia_Destruct(minutia);
