9c67d2063ffda897572ca836786f03bdb12f7b7c
Jeff Foster <jeff.foster@acm.org>
2015-11-05 15:47:26 +0000

    define a million and one structs

--- Sources/Matcher/FingerprintMatcher.h
+++ Sources/Matcher/FingerprintMatcher.h
@@ -1,4 +1,42 @@
 #ifndef _FINGERPRINTMATCHER_H_
 #define _FINGERPRINTMATCHER_H_
 
+struct MinutiaPair
+{
+  int probe;
+  int candidate;
+};
+
+struct EdgePair
+{
+  struct MinutiaPair reference;
+  struct MinutiaPair neighbour;
+};
+
+struct PairInfo
+{
+  struct MinutiaPair pair;
+  struct MinutiaPair reference;
+  int supportingEdges;
+};
+
+struct IndexedEdge
+{
+  //  EdgeShape shape;
+  int reference;
+  int neighbour;
+};
+
+struct FingerprintMatcher
+{
+  // TODO: dictionary
+
+  // TODO: priority queue (float)
+
+  struct PairInfo* pairsByCandidate;
+  struct PairInfo* pairsByProbe;
+  struct PairInfo* pairLife;
+  int pairCount;  
+};
+  
 #endif
