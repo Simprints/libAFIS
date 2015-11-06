19a7c626c087da16b5bb467210c90bee3b3f2a09
cjheppell <chris.john.heppell@gmail.com>
2015-11-06 15:09:53 +0000

    Remove the unused variable

--- Sources/Extraction/Model/DotRemover.c
+++ Sources/Extraction/Model/DotRemover.c
@@ -6,18 +6,17 @@
 
 void DotRemover_Filter(List *minutiae)
 {
-    int count = 0; 
     for (ListElement *i = minutiae->head, *next = NULL; i != NULL; i = next)
     {
         Minutia *minutia = (Minutia *) i->data;
         assert(minutia != NULL);
 
         //Get the next element here as i may be freed in List_Remove
-        next = i->next; 
+        next = i->next;
         if (List_GetCount(Minutia_GetRidges(minutia)) == 0)
         {
              int32_t ret = List_Remove(minutiae, i, NULL);
              assert(ret == 0);
-        } 
+        }
     }
 }
