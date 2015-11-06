090b751eb9652a9af47b7276ec2d1772cf052afe
Arun Kumar <arun.kumar@red-gate.com>
2015-11-06 14:27:57 +0000

    Fixed formatting

--- Sources/General/Angle.c
+++ Sources/General/Angle.c
@@ -3,6 +3,18 @@
 
 static const double PI = 3.14159265358979323846;
 
+static float Angle_atan(double x, double y) 
+{
+    double result = atan2(y, x); 
+
+    if (result < 0) 
+    {
+        result += 2 * PI; 
+    }
+
+    return result;
+}
+
 float Angle_FromFraction(float fraction)
 {
     return fraction * 2 * PI;
@@ -23,16 +35,6 @@ PointF Angle_ToVector(float angle)
     return PointF_Construct(cos(angle), sin(angle));
 }
 
-static float Angle_atan(double x, double y) {
-    double result = atan2(y, x); 
-
-    if (result < 0) {
-        result += 2 * PI; 
-    }
-
-    return result;
-}
-
 float Angle_Atan(Point p) 
 {
      return Angle_atan(p.x, p.y); 
@@ -42,16 +44,19 @@ float Angle_AtanF(PointF p) {
     return Angle_atan(p.x, p.y); 
 }
 
-float Angle_ToOrientation(float direction) {
+float Angle_ToOrientation(float direction) 
+{
     if (direction < PI) return 2 * direction; 
 
     return 2 * (direction - PI); 
 }
 
-float Angle_Add(float angle1, float angle2) {
+float Angle_Add(float angle1, float angle2) 
+{
     float result = angle1 + angle2; 
 
-    if (result < (2 * PI)) {
+    if (result < (2 * PI)) 
+    {
         return result; 
     }
 
