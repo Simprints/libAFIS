8041f527c81ce8a66b3c7408df6e58f72a95a159
Arun Kumar <arun.kumar@red-gate.com>
2015-11-06 15:59:45 +0000

    Uncommented commented out tests

--- Sources/Tests/Extraction/Filters/runners/TestRunner_HillOrientation.c
+++ Sources/Tests/Extraction/Filters/runners/TestRunner_HillOrientation.c
@@ -3,7 +3,7 @@
 
 TEST_GROUP_RUNNER(HillOrientation)
 {
-    //RUN_TEST_CASE(HillOrientation, VisualiseOrientations);
-    //RUN_TEST_CASE(HillOrientation, VisualisePixelMask);
+    RUN_TEST_CASE(HillOrientation, VisualiseOrientations);
+    RUN_TEST_CASE(HillOrientation, VisualisePixelMask);
     RUN_TEST_CASE(HillOrientation, ComputeOrientations);
 }
